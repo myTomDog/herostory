18707fab685ac9b560a3ea23db8067a068801235
欢迎来到我的首页 <2425859797@qq.com>
2019-12-09 21:16:11 +0800

    第三节课代码分支新建

--- src/main/java/org/tinygame/herostory/GameMsgDecoder.java
+++ src/main/java/org/tinygame/herostory/GameMsgDecoder.java
@@ -1,19 +1,22 @@
 package org.tinygame.herostory;
 
-import com.google.protobuf.GeneratedMessageV3;
+import com.google.protobuf.Message;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
-import org.tinygame.herostory.msg.GameMsgProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 消息解码器
- *
- * @author Administrator
- * @date 2019/12/6
  */
 public class GameMsgDecoder extends ChannelInboundHandlerAdapter {
+    /**
+     * 日志对象
+     */
+    static private Logger LOGGER = LoggerFactory.getLogger(GameMsgDecoder.class);
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (!(msg instanceof BinaryWebSocketFrame)) {
@@ -24,35 +27,28 @@ public class GameMsgDecoder extends ChannelInboundHandlerAdapter {
         BinaryWebSocketFrame frame = (BinaryWebSocketFrame) msg;
         ByteBuf byteBuf = frame.content();
 
-        // 读取消息的长度
-        byteBuf.readShort();
-        // 读取消息的编号
-        int msgCode = byteBuf.readShort();
+        byteBuf.readShort(); // 读取消息的长度
+        int msgCode = byteBuf.readShort(); // 读取消息的编号
+
+        // 获取消息构建者
+        Message.Builder msgBuilder = GameMsgRecognizer.getBuilderByMsgCode(msgCode);
+        if (null == msgBuilder) {
+            LOGGER.error("无法识别的消息, msgCode = {}", msgCode);
+            return;
+        }
 
         // 拿到消息体
         byte[] msgBody = new byte[byteBuf.readableBytes()];
         byteBuf.readBytes(msgBody);
 
-        GeneratedMessageV3 cmd = null;
+        msgBuilder.clear();
+        msgBuilder.mergeFrom(msgBody);
 
-        switch (msgCode) {
-            case GameMsgProtocol.MsgCode.USER_ENTRY_CMD_VALUE:
-                cmd = GameMsgProtocol.UserEntryCmd.parseFrom(msgBody);
-                break;
+        // 构建消息
+        Message newMsg = msgBuilder.build();
 
-            case GameMsgProtocol.MsgCode.WHO_ELSE_IS_HERE_CMD_VALUE:
-                cmd = GameMsgProtocol.WhoElseIsHereCmd.parseFrom(msgBody);
-                break;
-
-            case GameMsgProtocol.MsgCode.USER_MOVE_TO_CMD_VALUE:
-                cmd = GameMsgProtocol.UserMoveToCmd.parseFrom(msgBody);
-                break;
-
-            default:
-        }
-
-        if (null != cmd) {
-            ctx.fireChannelRead(cmd);
+        if (null != newMsg) {
+            ctx.fireChannelRead(newMsg);
         }
     }
 }
